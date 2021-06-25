11327ac3e7015d312296e8773a1ba6c60ce012c7
ghozi <ghozifr@gmail.com>
2021-06-25 08:35:39 +0800

    upload1

---
+++ 05-assignmentsOperator/src/com/index.java
@@ -0,0 +1,21 @@
+package com;
+
+public class index {
+    public static void main(String[] args){
+        
+        // Assignment oprtator
+
+        // Assignment operator addition
+        int a = 1;
+        a += 10 // a = a + 10;
+        System.out.println("value of a = " + a);
+
+        // Assignment operator subtraction
+        int b = 100;
+        b -= 25; // b = b - 25;
+        System.out.println("value of b = " + b);
+
+        // Assignmet operator
+        
+    }
+}
